762564e736dc4f1ad173f5e772f1ff00cdd3e669
spineda2019 <spineda.wpi.alum@gmail.com>
2024-05-16 09:11:32 -0400

    stub basic arg parsing (help && version)

--- cnew/main.cpp
+++ cnew/main.cpp
@@ -16,8 +16,50 @@
  *  with this program. If not, see <https://www.gnu.org/licenses/>
  */
 
+#include <argparse/argparse.hpp>
 #include <iostream>
-int main(void) {
-    std::cout << "Hi!" << std::endl;
+
+int main(int argc, char** argv) {
+    constexpr const char* version{"0.0.1"};
+    constexpr const char* program{"CNew"};
+
+    argparse::ArgumentParser argument_parser{program, version,
+                                             argparse::default_arguments::none};
+
+    argument_parser.add_argument("-h", "--help")
+        .help("Print Help Message & Exit")
+        .flag();
+
+    argument_parser.add_argument("-v", "--version")
+        .help("Display Program Version")
+        .flag();
+
+    try {
+        argument_parser.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << argument_parser;
+        return 1;
+    }
+
+    if (argument_parser.get<bool>("-h")) {
+        std::cout << argument_parser;
+        return 0;
+    } else if (argument_parser.get<bool>("-v")) {
+        std::cout << program << " " << version << std::endl;
+        std::cout << "Copyright (c) 2024 Sebastian Pineda "
+                     "(spineda.wpi.alum@gmail.com)"
+                  << std::endl;
+        std::cout
+            << "This program is free software; you may redistribute it under "
+               "the "
+               "terms of the\nGNU General Public License version 2 or (at your "
+               "option) any later version. This\nprogram has absolutely no "
+               "warranty."
+            << std::endl
+            << std::endl;
+        return 0;
+    }
+
     return 0;
 }
